9da50b64ce9034d08e6ed3486feb99d44d530ed3
Kevinwmiguel <kevincursos@outlook.com>
2026-07-25 18:13:34 +0100

    Done

--- ex02/RobotomyRequestForm.cpp
+++ ex02/RobotomyRequestForm.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kwillian <kwillian@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kevin <kevin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 00:00:30 by kwillian          #+#    #+#             */
-/*   Updated: 2026/06/23 11:19:16 by kwillian         ###   ########.fr       */
+/*   Updated: 2026/07/25 15:56:28 by kevin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void RobotomyRequestForm::executeAction() const
 {
     std::cout << "Bzzzz... drilling noises..." << std::endl;
     if (std::rand() % 2)
-        std::cout << _target << " has been robotomized successfully." << std::endl;
+        std::cout << _target << " has been robotomized successfully 50% of the time" << std::endl;
     else
         std::cout << _target << " robotomy failed." << std::endl;
 }
